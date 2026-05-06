cf24481b50d7d01d62b4d1b9292a0c66596f7c0c
arda-tekinarslan <tekinarslan.arda11@gmail.com>
2026-05-06 12:02:40 +0300

    svd.py improved

--- svd.py
+++ svd.py
@@ -25,18 +25,13 @@ def power_iteration(A, tol=1e-6, max_iter=1000):
     return eigenVal, randVec
 
 
-if __name__ == "__main__":
-    A = np.array([[2, 1], [1, 2]])
-    val, vec = power_iteration(A)
-    print("Test eigenvalue:", val)
-    print("Test eigenvector:", vec)
-
-
 def deflation(A):
     pca_pairs = []
     for i in range(A.shape[0]):
         val, vec = power_iteration(A)
         pca_pairs.append((val, vec))
+        # Small mistakes lead to larger problems
+        vec = vec / np.linalg.norm(vec)
         A = A - val * np.outer(vec, vec)
     return pca_pairs
 
@@ -50,19 +45,35 @@ def full_svd(A):
 
     # A = UEV^T
     V = np.zeros((n_cols, n_cols))
-    E = np.zeros((n_rows, n_cols))
-    U = np.zeros((n_rows, n_rows))
+    E = np.zeros((n_cols, n_cols))
+    U = np.zeros((n_rows, n_cols))
 
     for i in range(n_cols):
         val_i, vec_i = pca_pairs[i]
+        vec_i = vec_i / np.linalg.norm(vec_i)
 
-        sigma_i = np.sqrt(abs(val_i))
+        # np.sqrt(max(val_i, 0)) you could use
+        sigma_i = np.sqrt(max(val_i, 0))  # ???????
         E[i, i] = sigma_i  # Diagonals of E
         V[:, i] = vec_i  # Columns of V are eigenvectors
 
         if sigma_i > 1e-8:  # Fixing the zero division possibility
             u_i = (A @ vec_i) / sigma_i
+            u_i = u_i / np.linalg.norm(u_i)
             U[:, i] = u_i
         else:
             U[:, i] = np.zeros(n_rows)
     return U, E, V.T
+
+
+if __name__ == "__main__":
+    print("--- Power Iteration Test ---")
+    A_test = np.array([[2, 1], [1, 2]])
+    val, vec = power_iteration(A_test)
+    print("Test eigenvalue:", val)
+    print("Test eigenvector:", vec)
+
+    print("\n--- SVD Test ---")
+    U, E, Vt = full_svd(A_test)
+    A_rec = U @ E @ Vt
+    print("Reconstruction error:", np.linalg.norm(A_test - A_rec))
