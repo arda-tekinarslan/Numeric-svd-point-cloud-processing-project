5efbb0dc2d4a05e9e2697c8713432a1a035e51e8
arda-tekinarslan <tekinarslan.arda11@gmail.com>
2026-05-09 16:36:25 +0300

    Colorizing normals implemented succesfully

--- surface-normals.py
+++ surface-normals.py
@@ -28,7 +28,23 @@ for i in range(points.shape[0]):
         normalVecs[i] = normalVecs[i] * -1
 
 pcd.normals = o3d.utility.Vector3dVector(normalVecs)
-nColors = (normalVecs + 1.0) / 2.0
-pcd.colors = o3d.utility.Vector3dVector(nColors)
-o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+pColors = (normalVecs + 1.0) / 2.0
+pcd.colors = o3d.utility.Vector3dVector(pColors)
+
+normalVecLen = 0.007  # Scaling normal vectors
+
+lineStarts = points
+lineEnds = points + (normalVecs * normalVecLen)
+# vstack first place starts then ends
+linePoints = np.vstack([lineStarts, lineEnds])
+N = points.shape[0]  # i and i + N makes a line
+lineIndices = np.array([[i, i+N] for i in range(N)])  # i and i + N is a set
+
+lineSet = o3d.geometry.LineSet()
+lineSet.points = o3d.utility.Vector3dVector(linePoints)
+lineSet.lines = o3d.utility.Vector2iVector(lineIndices)
+lineSet.colors = o3d.utility.Vector3dVector(pColors)
+
+
+o3d.visualization.draw_geometries([pcd, lineSet])
 o3d.io.write_point_cloud("output/bunny_normals.pcd", pcd)
