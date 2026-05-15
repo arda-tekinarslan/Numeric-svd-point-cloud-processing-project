compare_svd.py.implemented and some numerical errors fixed

curvature.py
@@ -17,9 +17,7 @@ for i in range(points.shape[0]):
 
     meanCentre = np.mean(neighbors, axis=0)
     centredNeighbors = neighbors - meanCentre
-
-    neighborCov = centredNeighbors.T @ centredNeighbors
-    U, E, Vt = full_svd(neighborCov)  # E is 3x3 matrix
+    U, E, Vt = full_svd(centredNeighbors)  # E is 3x3 matrix
 
     eigen1 = E[0, 0]
     eigen2 = E[1, 1]

obb.py
@@ -9,10 +9,7 @@ points = np.asarray(pcd.points)  # Use as matrix
 meanCentre = np.mean(points, axis=0)  # Mean of every colum
 # Now points centered (0,0) PCA works according to origin
 centeredPoints = points - meanCentre
-N = centeredPoints.shape[0]
-covariance = (centeredPoints.T @ centeredPoints) / (N-1)
-U, E, Vt = full_svd(covariance)
-
+U, E, Vt = full_svd(centeredPoints)
 principleCom = Vt
 
 # Projected to the principle components

surface-normals.py
@@ -19,10 +19,7 @@ for i in range(points.shape[0]):
 
     meanCentre = np.mean(neighbors, axis=0)
     centredNeigbors = neighbors - meanCentre
-    N = centredNeigbors.shape[0]
-
-    neighborCov = (centredNeigbors.T @ centredNeigbors) / (N-1)
-    U, E, Vt = full_svd(neighborCov)
+    U, E, Vt = full_svd(centredNeigbors)
     normalVecs[i] = Vt[-1]  # Smallest sigma last row vector of Vt
 
     if (np.dot(points[i] - centroid, normalVecs[i]) < 0):

svd.py
@@ -3,7 +3,7 @@ import numpy as np
 _RNG = np.random.default_rng(42)
 
 
-def power_iteration(A, tol=1e-6, max_iter=1000):
+def power_iteration(A, tol=1e-8, max_iter=1000):
 
     n = A.shape[0]
     randVec = _RNG.standard_normal(n)  # Gaussian standart normal
@@ -23,23 +23,29 @@ def power_iteration(A, tol=1e-6, max_iter=1000):
 
         randVec = newVec
 
-    # You can use dot product also for this equation
-    eigenVal = np.dot(randVec, (A @ randVec))
-    return eigenVal, randVec
+    eigenVal = np.dot(randVec, (A @ randVec))  # randVec.T @ A @ randVec
+    return eigenVal, randVec  # Dominant eigenvalue and eigenvector
 
 
 def deflation(A):
     pcaPairs = []
+    oldVecs = []
+    C = A.copy()
     for i in range(A.shape[0]):
-        val, vec = power_iteration(A)
+        val, vec = power_iteration(C)
+        # Applyin modified gram-schmidt(better) to make sure all orthogonal
+        for o in oldVecs:
+            vec = vec - (vec @ o) * o
+        vec = vec / np.linalg.norm(vec)  # Normalize everytime
+        oldVecs.append(vec)
         pcaPairs.append((val, vec))
-        vec = vec / np.linalg.norm(vec)
-        A = A - val * np.outer(vec, vec)
+        C = C - val * np.outer(vec, vec)  # vec @ vec.T is a matrix
     return pcaPairs
 
 
 def full_svd(A):
-    X = A.T @ A
+    N = A.shape[0]
+    X = (A.T @ A) / (N-1)
     pcaPairs = deflation(X)
     pcaPairs.sort(key=lambda x: x[0], reverse=True)
     nRows = A.shape[0]
@@ -49,20 +55,23 @@ def full_svd(A):
     V = np.zeros((nCols, nCols))
     E = np.zeros((nCols, nCols))
     U = np.zeros((nRows, nCols))
-
+    prevU = []
     for i in range(nCols):
         val_i, vec_i = pcaPairs[i]
         vec_i = vec_i / np.linalg.norm(vec_i)
-
-        # np.sqrt(max(val_i, 0)) you could use
-        sigma_i = np.sqrt(max(val_i, 0))
+        # AV =,UE
+        r_i = A @ vec_i
+        sigma_i = np.linalg.norm(r_i)
         E[i, i] = sigma_i  # Diagonals of E
         V[:, i] = vec_i  # Columns of V are eigenvectors
 
         if sigma_i > 1e-8:  # Fixing the zero division possibility
-            u_i = (A @ vec_i) / sigma_i
+            u_i = r_i / sigma_i
+            for pU in prevU:
+                u_i = u_i - (u_i @ pU) * pU
             u_i = u_i / np.linalg.norm(u_i)
             U[:, i] = u_i
+            prevU.append(u_i)
         else:
             U[:, i] = np.zeros(nRows)
     return U, E, V.T